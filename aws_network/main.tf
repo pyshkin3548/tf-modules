b0241c98c03993faabd40c8cb194a3e7b6532c8d
pyshkin <pyshkin3548@gmail.com>
2022-11-04 15:39:19 -0700

    Module to Create AWS VPC Networks

---
+++ aws_network/main.tf
@@ -0,0 +1,130 @@
+#----------------------------------------------------------
+# Terraform
+# Provisoin:
+#
+# At Remote State on S3
+# - VPC
+# - Internet Gateway
+# - XX Public Subnets
+# - XX Private Subnets
+# - XX NAT Gateways in Public Subnets to give access to Internet from Private Subnets
+#
+#----------------------------------------------------------
+
+# For module we do not need to add provider and remote folder at this code
+/*
+provider "aws" {
+  region = "ca-central-1"
+}
+*/
+
+/*
+terraform {
+  backend "s3" {
+    bucket = "pyshkin-tf-state"                       # Best practise to use on bucket for one project 
+    key    = "examples/dev/network/terraform.tfstate" # For each part of this project need to use unique path/key
+    region = "ca-central-1"
+  }
+}
+*/
+
+#=========================================================
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-VPC"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-IGW"
+  }
+}
+
+#-----------------Public Subnets and Routing
+
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index] # 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env}-public-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.env}-route-public-subnets"
+  }
+}
+
+resource "aws_route_table_association" "public_routes" {
+  count          = length(aws_subnet.public_subnets[*].id)
+  route_table_id = aws_route_table.public_subnets.id
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+}
+
+
+#-----------------NAT GW with Elastic IPs----------------------
+
+resource "aws_eip" "nat" {
+  count = length(var.private_subnet_cidrs)
+  vpc   = true
+  tags = {
+    Name = "${var.env}-nat-gw-${count.index + 1}"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.private_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
+  tags = {
+    Name = "${var.env}-nat-gw-${count.index + 1}"
+  }
+}
+
+#--------------Private Subnet and Routing----------------------
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "${var.env}-private-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "private_subnets" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+  tags = {
+    Name = "${var.env}-route-private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private_routes" {
+  count          = length(aws_subnet.private_subnets[*].id)
+  route_table_id = aws_route_table.private_subnets[count.index].id
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+}
+
+#=========================================================================
